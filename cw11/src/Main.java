5da190eaf79186245e5a335aba2fca1ad4ce93ae
s19191 <s19191@pjwstk.edu.pl>
2020-05-31 16:15:08 +0200

    zrobione, rzeczyiście 10 min

--- cw11/src/Main.java
+++ cw11/src/Main.java
@@ -2,5 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        double x;
+        double y;
+        Integer n = 200;
+        int no = 0;
+        for (int i = 0; i < n; i++) {
+            x = Math.random() * Math.PI;
+            y = Math.random();
+            if (Math.sin(x) > y) {
+                no++;
+            }
+        }
+        double P = Math.PI * no/n;
+        System.out.println("Pole wynosi: " + P);
     }
 }
